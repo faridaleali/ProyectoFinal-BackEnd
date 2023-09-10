1a7f644c1bb907e5578786a99115ef00893ed1ea
Nancylug <nancylug@yahoo.com>
2023-09-09 22:49:03 -0300

    agregando schemas Categorias y rol

---
+++ src/models/categorias.js
@@ -0,0 +1,9 @@
+const {Schema, model} = require('mongoose');
+
+const CategoriaSchema = Schema({
+    nombre: {type: String, required: [true, 'El nombre es obligatorio'], unique: true},
+    estado:{type: Boolean, required: true, default: true},
+    usuario: {type: Schema.Types.ObjectId, ref: "Usuario", required: true}
+});
+
+module.exports = model("Categoria", CategoriaSchema);
