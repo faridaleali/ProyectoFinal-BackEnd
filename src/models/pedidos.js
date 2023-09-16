e16337c0064199984e5ecf06bcdb8f3ac5baf7e8
MilagroR27 <rodriguezmm05@hotmail.com>
2023-09-15 22:30:23 -0300

    modificando pedidos/models

--- src/models/pedidos.js
+++ src/models/pedidos.js
@@ -2,12 +2,15 @@ const {Schema, model}= require('mongoose')
 
 // Aqui se especifica que se pide para pedidos
 
-const PedidoSchema= Schema({
+const OrderSchema= Schema({
   user:{type: Schema.Types.ObjectId, ref:"usuario"},
-  date: {type: Date, required:[true, 'La Fecha es obligatoria'],default: Date.now },
-  order:{type: String, required:[true, 'La Orden es obligatoria']},
-  status:{type: String, default: 'Pendiente'},
+  date: {type: Date,default: Date.now },
+  order:{type:String, required:[true]},
+  status: {type: String, enum: ['pendiente', 'realizado'], default: 'pendiente', required: true},
   totalCost:{type: Number, required:[true]}
 })
 
-module.exports=model("pedido", PedidoSchema)
+// enum especifica que solo puede contener uno de los siguientes valores: 'pendiente' o 'realizado', viene de moongose
+// La restricción enum es útil cuando deseas asegurarte de que los valores de un campo sean específicos y se ajusten a un conjunto limitado de opciones, lo que puede ayudar a mantener la integridad de los datos y prevenir entradas no válidas en tu base de datos.
+
+module.exports=model("Order", OrderSchema)
